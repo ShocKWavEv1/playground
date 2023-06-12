f45ccb71eed0efc09767100c127dd857115ff84e
rodd.sal <rodrigosalmeron@tuhabi.mx>
2023-06-11 23:55:36 -0600

    playground starter

---
+++ next.config.js
@@ -0,0 +1,117 @@
+/** @type {import('next').NextConfig} */
+
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+  webpack: (config, options) => {
+    const { dir } = options
+
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              memo: true,
+              dimensions: false,
+              svgoConfig: {
+                multipass: true,
+                plugins: [
+                  'removeDimensions',
+                  'removeOffCanvasPaths',
+                  'reusePaths',
+                  'removeElementsByAttr',
+                  'removeStyleElement',
+                  'removeScriptElement',
+                  'prefixIds',
+                  'cleanupIDs',
+                  {
+                    name: 'cleanupNumericValues',
+                    params: {
+                      floatPrecision: 1,
+                    },
+                  },
+                  {
+                    name: 'convertPathData',
+                    params: {
+                      floatPrecision: 1,
+                    },
+                  },
+                  {
+                    name: 'convertTransform',
+                    params: {
+                      floatPrecision: 1,
+                    },
+                  },
+                  {
+                    name: 'cleanupListOfValues',
+                    params: {
+                      floatPrecision: 1,
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    )
+
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ['raw-loader', 'glslify-loader'],
+    })
+
+    config.plugins.push(new DuplicatePackageCheckerPlugin())
+
+    return config
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ]
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+}
+module.exports = () => {
+  const plugins = [withBundleAnalyzer]
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...nextConfig,
+  })
+}
